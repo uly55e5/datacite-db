0807fc08c0d9a52515cc5d372728dd31c255cc65
uly55e5 <uly55e5@web.de>
2021-10-13 08:20:16 +0200

    parallel functions

---
+++ datacite/datacite.go
@@ -0,0 +1,128 @@
+package datacite
+
+import (
+	"bufio"
+	"context"
+	"encoding/json"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"golang.org/x/sync/semaphore"
+	"os"
+	"time"
+)
+
+type Count struct {
+	Upserted int64
+	Modified int64
+}
+
+func SendReplaceModel(dataset interface{}, modelChan chan *mongo.ReplaceOneModel, sem *semaphore.Weighted) {
+	id := dataset.(map[string]interface{})["id"]
+	updated := dataset.(map[string]interface{})["attributes"].(map[string]interface{})["updated"]
+	var filter = bson.D{{"id", id}, {"attributes.updated", bson.M{"$lt": updated}}}
+	model := mongo.NewReplaceOneModel().SetUpsert(true).SetReplacement(dataset).SetFilter(filter)
+	modelChan <- model
+	sem.Release(1)
+}
+
+func AddModels(m chan *mongo.ReplaceOneModel, collection *mongo.Collection, cCount chan Count) {
+	var (
+		models = make([]mongo.WriteModel, 10000, 10000)
+		err    error
+		ok     bool
+		i      = 0
+	)
+	for {
+		models[i] = <-m
+		i++
+		if i == 10000 {
+			var bulkErr mongo.BulkWriteException
+			var res *mongo.BulkWriteResult
+			inCtx, inCancel := context.WithTimeout(context.Background(), 60*time.Second)
+			inOptions := options.BulkWrite().SetOrdered(false)
+			if res, err = collection.BulkWrite(inCtx, models, inOptions); err != nil {
+				print("*")
+				if bulkErr, ok = err.(mongo.BulkWriteException); ok {
+					for _, writeError := range bulkErr.WriteErrors {
+						go func(writeError mongo.BulkWriteError) {
+							if writeError.Code != 11000 {
+								println(writeError.Message)
+							}
+						}(writeError)
+					}
+				} else {
+					println(err.Error())
+				}
+			} else {
+				print(".")
+			}
+			if res != nil {
+				cCount <- Count{res.UpsertedCount, res.ModifiedCount}
+			}
+			i = 0
+			inCancel()
+		}
+	}
+}
+
+func LogCount(count Count, cCount chan Count) {
+	var lastCount int64 = 0
+	var lastModified int64 = 0
+	println(count.Upserted, count.Modified)
+	for {
+		newCount := <-cCount
+		count.Upserted += newCount.Upserted
+		count.Modified += newCount.Modified
+		if count.Upserted-lastCount > 100000 || count.Modified-lastModified > 100000 {
+			println(count.Upserted, count.Modified)
+			lastCount = count.Upserted
+			lastModified = count.Modified
+		}
+	}
+}
+
+func ReadLine(lineReader *bufio.Scanner, sem *semaphore.Weighted, modelChan chan *mongo.ReplaceOneModel) {
+	var err error
+	line := lineReader.Bytes()
+	if len(line) < 1 {
+		return
+	}
+	var v map[string]interface{}
+	if err = json.Unmarshal(line, &v); err != nil {
+		println(err.Error())
+		return
+	}
+	if data, ok := v["data"]; ok {
+		dataArray := data.([]interface{})
+		for _, dataset := range dataArray {
+			if err = sem.Acquire(context.Background(), 1); err != nil {
+				println("Could not acquire semaphore", err.Error())
+			}
+			go SendReplaceModel(dataset, modelChan, sem)
+		}
+	}
+	return
+}
+
+func ReadFile(fileName string, sem *semaphore.Weighted, modelChan chan *mongo.ReplaceOneModel, fileSem *semaphore.Weighted) {
+	var file *os.File
+	var err error
+	file, err = os.Open(fileName)
+	if err != nil {
+		print("File open error:", err.Error())
+		return
+	}
+	lineReader := bufio.NewScanner(file)
+	const maxLineLength = 2000000
+	buf := make([]byte, maxLineLength)
+	lineReader.Buffer(buf, maxLineLength)
+	for lineReader.Scan() {
+		ReadLine(lineReader, sem, modelChan)
+	}
+	if err = file.Close(); err != nil {
+		println("Could not close file ", fileName, err.Error())
+	}
+	fileSem.Release(1)
+	return
+}
--- dc-dbtool/main.go
+++ dc-dbtool/main.go
@@ -1,37 +1,39 @@
 package main
 
 import (
-	"bufio"
 	"context"
-	"encoding/json"
+	"datacite-db/datacite"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"golang.org/x/sync/semaphore"
 	"io/fs"
 	"io/ioutil"
-	"os"
 	"time"
 )
 
 const (
-	dbConnStr = "mongodb://datacite:datacite@localhost:27017"
-	dataDir   = "/home/uly55e5/Projekte/ipb/dcdump/temp-20210917/"
+	dbConnStr         = "mongodb://datacite:datacite@localhost:27017"
+	dataDir           = "/home/uly55e5/Projekte/ipb/dcdump/temp-20210917/"
+	parallelFiles     = 50
+	parallelDatasets  = 500
+	dataSetBufferSize = 10000
+	countBufferSize   = 100
+	maxDbWorkers      = 3
+	dbName            = "datacite-go"
 )
 
 func main() {
 
 	var (
-		count        int64 = 0
-		lastcount    int64 = 0
-		modified     int64 = 0
-		lastmodified int64 = 0
-		client       *mongo.Client
-		err          error
-		files        []fs.FileInfo
-		collection   *mongo.Collection
-		models       []mongo.WriteModel = nil
+		client     *mongo.Client
+		err        error
+		files      []fs.FileInfo
+		collection *mongo.Collection
 	)
+
+	var count = datacite.Count{}
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	if client, err = mongo.Connect(ctx, options.Client().ApplyURI(dbConnStr)); err != nil {
@@ -50,93 +52,46 @@ func main() {
 		return
 	}
 
-	collection = client.Database("datacite-go").Collection("datacite")
-	indexName, err := collection.Indexes().CreateOne(
-		context.TODO(),
-		mongo.IndexModel{
-			Keys:    bson.D{{Key: "id", Value: 1}},
-			Options: options.Index().SetUnique(true),
-		},
-	)
+	var idModels []mongo.IndexModel
+	idIndexModel := mongo.IndexModel{
+		Keys:    bson.D{{Key: "id", Value: 1}},
+		Options: options.Index().SetUnique(true),
+	}
+	idModels = append(idModels, idIndexModel)
+	updateIndexModel := mongo.IndexModel{
+		Keys:    bson.D{{"attributes.updated", 1}},
+		Options: options.Index(),
+	}
+	idModels = append(idModels, updateIndexModel)
+
+	collection = client.Database(dbName).Collection("datacite")
+	var names []string
+	names, err = collection.Indexes().CreateMany(context.Background(), idModels)
 	if err != nil {
 		println("Error while creating index:", err.Error())
 	}
-	println("Index:", indexName)
+	println("Index:", names)
 
 	if files, err = ioutil.ReadDir(dataDir); err != nil {
 		println("Read error:", err.Error())
 		return
 	}
 
-	count, err = collection.CountDocuments(context.TODO(), bson.D{}, nil)
+	count.Upserted, err = collection.CountDocuments(context.Background(), bson.D{}, nil)
 
-	for _, fileinfo := range files {
-		var file *os.File
-		file, err = os.Open(dataDir + fileinfo.Name())
-		if err != nil {
-			print("File open error:", err.Error())
-			continue
-		}
-		lineReader := bufio.NewScanner(file)
-		const maxLineLength = 2000000
-		buf := make([]byte, maxLineLength)
-		lineReader.Buffer(buf, maxLineLength)
-		for lineReader.Scan() {
-			line := lineReader.Bytes()
-			if len(line) < 1 {
-				continue
-			}
-			var v map[string]interface{}
-			if err = json.Unmarshal(line, &v); err != nil {
-				println(err.Error())
-				continue
-			}
-			if data, ok := v["data"]; ok {
-				dataarray := data.([]interface{})
-				if len(dataarray) > 0 {
-					for _, dataset := range dataarray {
-						id := dataset.(map[string]interface{})["id"]
-						updated := dataset.(map[string]interface{})["attributes"].(map[string]interface{})["updated"]
-						var filter = bson.D{{"id", id}, {"attributes.updated", bson.M{"$lt": updated}}}
-						model := mongo.NewReplaceOneModel().SetUpsert(true).SetReplacement(dataset).SetFilter(filter)
-						models = append(models, model)
-					}
-					if len(models) > 10000 {
-						var bulkErr mongo.BulkWriteException
-						var res *mongo.BulkWriteResult
-						inctx, incancel := context.WithTimeout(context.Background(), 5*time.Second)
-						defer incancel()
-						inOptions := options.BulkWrite().SetOrdered(false)
-						if res, err = collection.BulkWrite(inctx, models, inOptions); err != nil {
-							print("*")
-							if bulkErr, ok = err.(mongo.BulkWriteException); ok {
-								for _, berr := range bulkErr.WriteErrors {
-									go func(berr mongo.BulkWriteError) {
-										if berr.Code != 11000 {
-											println(berr.Message)
-										}
-									}(berr)
-								}
-							} else {
-								println(err.Error())
-							}
-						} else {
-							print(".")
-							modified += res.ModifiedCount
-							count += res.UpsertedCount
-						}
-						models = nil
-					}
-				}
-				if count-lastcount > 100000 || modified-lastmodified > 100000 {
-					println(count, modified)
-					lastcount = count
-					lastmodified = modified
-				}
-			}
-
-		}
-		file.Close()
+	var modelChan = make(chan *mongo.ReplaceOneModel, dataSetBufferSize)
+	var countChan = make(chan datacite.Count, countBufferSize)
+	for i := 0; i < maxDbWorkers; i++ {
+		go datacite.AddModels(modelChan, collection, countChan)
 	}
-	println(count, modified)
+	go datacite.LogCount(count, countChan)
+	sem := semaphore.NewWeighted(parallelDatasets)
+	fileSem := semaphore.NewWeighted(parallelFiles)
+	for _, fileInfo := range files {
+		if err = fileSem.Acquire(context.Background(), 1); err != nil {
+			println("Could not acquire file semaphore.", err.Error())
+		}
+		datacite.ReadFile(dataDir+fileInfo.Name(), sem, modelChan, fileSem)
+	}
+	println(count.Upserted, count.Modified)
 }
